569b2f307b9c6ea8e97ed7e9b725248294be6ef5
Hòa Đinh Quang <hoaquang>
2025-11-22 17:58:58 +0700

    bai tap javascript

---
+++ js/main.js
@@ -0,0 +1,5 @@
+console.log('bài tập 1: viết một hàm nhận 2 tham số a và b hàm trả về giá trị tồng a và b')
+
+console.log('bài tập 2: viết một hàm nhận tham số n nhập từ bàn phím, tính tổng(sum) từ 0 tới n và trả ra giá trị tổng')
+
+console.log('bài tập 3: viết một hàm nhận tham số n nhập từ bàn phím,hàm trả về giá trị là số nguyên tố từ 0 tới n')
