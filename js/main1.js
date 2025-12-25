4f7c13b0afa0ab030b8a6d7c0bfc1b731aa2aaa7
Hòa Đinh Quang <hoaquang>
2025-12-25 20:31:53 +0700

    bai tap moi thoi han mot tuan

---
+++ js/main1.js
@@ -0,0 +1,7 @@
+console.log(
+  "bài tập 1: tạo một đối tượng sinh viên với các thuộc tính tên, tuổi, điểm trung bình"
+);
+
+console.log(
+  "bài tập 2: Nhập vào một danh sách các sinh viên, mỗi sinh viên có tên, tuổi và điểm trung bình. Tính và in ra điểm trung bình của toàn bộ sinh viên."
+);
